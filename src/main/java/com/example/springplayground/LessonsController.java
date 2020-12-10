f54c718df51bd09b3e88f866e60bbf5c13187326
joelabq@gmail.com <joelabq@gmail.com>
2020-12-10 10:24:53 -0700

    CRUD endpoints

--- src/main/java/com/example/springplayground/LessonsController.java
+++ src/main/java/com/example/springplayground/LessonsController.java
@@ -2,6 +2,8 @@ package com.example.springplayground;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/lessons")
 public class LessonsController {
@@ -16,6 +18,18 @@ public class LessonsController {
     public Iterable<Lesson> all() {
 
         return this.repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Lesson> getById(@PathVariable String id) {
+
+        return this.repository.findById(Long.parseLong(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable String id) {
+
+         this.repository.deleteById(Long.parseLong(id));
     }
 
     @PostMapping("")
